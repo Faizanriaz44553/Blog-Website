3f679b2a05ba4ac40d9f7c510d1b90ef6b6f1f27
Faizanriaz44553 <sylanipractice@gmail.com>
2024-08-19 04:11:24 -0700

    project initialization

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -1,12 +1,17 @@
-import {db , collection , addDoc , getDocs } from "../firebase.js"
+import {db , collection , addDoc , getDocs ,signOut  ,auth , onAuthStateChanged} from "../firebase.js"
 
 let body = document.querySelector('body');
 let toggleButton = document.querySelector('#toggleButton');
 let lightFontColor = document.querySelector('#dark-mode-color');
-
-
-
-
+let creatModal = document.querySelector("#creatModal")
+let description = document.querySelector("#description")
+let tital = document.querySelector("#tital")
+let creatModalButton = document.querySelector("#creat-project")
+let hideicon = document.querySelector("#hideicon")
+let postDiv = document.querySelector("#post-div")
+let loaderCSS = document.querySelector("#loader")
+let LogoutButton = document.querySelector("#Logout-button")
+creatModal.style.display = "none"
 
 document.addEventListener("DOMContentLoaded", ()=> {
     toggleButton.addEventListener('click', function() {
@@ -16,38 +21,60 @@ document.addEventListener("DOMContentLoaded", ()=> {
     });
 });
 
-let creatModal = document.querySelector("#creatModal")
-creatModal.style.display = "none"
-let description = document.querySelector("#description")
-let plusIcon = document.querySelector("#plus-icon")
-let tital = document.querySelector("#tital")
-let creatModalButton = document.querySelector("#creat-project")
+
+
 const ceratUserFuntion = async ()=>{
+  loaderCSS.style.display = "block"
+  postFuntion()
+   if (tital.value.trim() === "") {
+    Swal.fire({
+      position: "top-center",
+      icon: "error",
+      title: "Oops...",
+      text: "Tital is empty",
+    });
+    console.log("tital is empty");
+    
+   } else if (description.value.trim() === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Decriptions is empty",
+    });
+   }
+   else {
+    postDiv.innerHTML = ""
+    creatModal.style.display = "none"
     try {
-        const docRef = await addDoc(collection(db, "post"), {
-          userTital: tital.value,
-          userDescription: description.value
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+      loaderCSS.style.display = "none"
+      const docRef = await addDoc(collection(db, "post"), {
+        userTital: tital.value,
+        userDescription: description.value
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    finally{
+      tital.value = ""
+      description.value = ""
+    }
+   }
 }
+creatModalButton.addEventListener("click" , ceratUserFuntion);
 
-function showcreatfuntion() {
-  creatModal.style.display = "block"
-}
-plusIcon.addEventListener("click" , showcreatfuntion)
 
-let postDiv = document.querySelector("#post-div")
+
 const postFuntion = async ()=>{
+  loaderCSS.style.display = "block"
     const querySnapshot = await getDocs(collection(db, "post"));
 querySnapshot.forEach((doc) => {
+  loaderCSS.style.display = "none"
     const {userDescription , userTital} = doc.data();
     postDiv.innerHTML += `<div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">${userTital}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
     <p class="card-text">${userDescription}</p>
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
@@ -57,6 +84,44 @@ console.log(userDescription , userTital);
 });
 }
 postFuntion()
-creatModalButton.addEventListener("click" , ceratUserFuntion);
+
+const logoutFuntion = ()=>{
+  signOut(auth).then(() => {
+    Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "Logout succesfuly",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        window.location.href = "../auth/signup.html"
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+LogoutButton.addEventListener("click" , logoutFuntion)
 
 
+
+ let plusIcon = document.querySelector("#plus-icon")
+function showcreatfuntion() {
+  creatModal.style.display = "block"
+}
+plusIcon.addEventListener("click" , showcreatfuntion)
+
+function hideModalCrossIcon() {
+  creatModal.style.display = "none"
+}
+hideicon.addEventListener("click" , hideModalCrossIcon)
+
+
+function stateChangedFuntion() {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      const uid = user.uid;
+      console.log(`state change nahi howa ${uid}`);
+      window.location.href = "../auth/signup.html"
+    }
+  });
+}
+stateChangedFuntion()
